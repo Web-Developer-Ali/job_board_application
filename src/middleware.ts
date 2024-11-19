ffc1297345546bd4aab373f7695c150f4da8e0b0
Ali hamza <148641299+Web-Developer-Ali@users.noreply.github.com>
2024-11-19 21:08:37 +0500

    log token issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
+  console.log(process.env.NEXTAUTH_SECRET)
+
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   console.log(token);
 
